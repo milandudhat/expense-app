4c045411544a32e56db001b1cc699173d85936c2
milanp <milan.dudhat@cybercom.in>
2023-09-19 14:27:48 +0530

    api changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Delete, Get, Post, Put, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Param, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ReportType, data } from './data';
+import { v4 } from "uuid";
+
+
 
 @Controller('report/:type')
 export class AppController {
@@ -12,23 +15,69 @@ export class AppController {
   ) {
     console.log(type);
     // return ["Report 1", "Report 2"];
-    const reportType = type === "income" ?  ReportType.INCOME : ReportType.EXPENSE;
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
     return data.report.filter(report => report.type === reportType);
   }
 
   @Get(":id")
-  getReportById() {
-    return {};
+  getReportById(
+    @Param("type") type: string,
+    @Param("id") id: string
+  ) {
+
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+
+    console.log(reportType);
+    console.log(id);
+
+    return data.report.filter(report => report.type === reportType).find(report => report.id === id) || "Report not found";
   }
 
   @Post()
-  createReport() {
-    return "Created";
+  createReport(
+    @Param("type") type: string,
+    @Body() body: {
+      source: string,
+      amount: number
+    }
+  ) {
+    // console.log(body);
+    const createReport = {
+      id: v4(),
+      source: body.source,
+      amount: body.amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    }
+
+    console.log(createReport);
+    data.report.push(createReport);
+    return createReport;
+
   }
 
   @Put(":id")
-  updateReport() {
-    return "Updated";
+  updateReport(
+    @Param("type") type: string,
+    @Param("id") id: string,
+    @Body() body: {
+      source: string,
+      amount: number
+    }
+  ) {
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+    const report = data.report.filter(report => report.type === reportType).find(report => report.id === id);
+    if(!report) return "Report not found";
+
+    const reportIndex = data.report.findIndex(report => report.id === id);
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    }
+
+    return data.report[reportIndex];
   }
 
   @Delete(":id")
