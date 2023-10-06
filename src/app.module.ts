b17aede1a6eaba3a1a66a8668b71fce5ab753e69
milanp <milan.dudhat@cybercom.in>
2023-10-06 18:17:22 +0530

    api changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CustomInterceptor} from './custom.interceptor';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService , {
     provide : APP_INTERCEPTOR,
-    useClass : ClassSerializerInterceptor
+    // useClass : ClassSerializerInterceptor
+    useClass : CustomInterceptor
   }],
 })
 export class AppModule {}
--- src/custom.interceptor.ts
+++ src/custom.interceptor.ts
@@ -1,8 +1,35 @@
-const { NestInterceptor } = require('@nestjs/common');
+import { ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { NestInterceptor, Injectable } from '@nestjs/common';
+import { map } from 'rxjs/operators';
 
+@Injectable()
+export class CustomInterceptor implements NestInterceptor {
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ): Observable<any> {
 
-class CustemInterceptor implements NestInterceptor {
-    intercept(context, next) {
-        return next.handle();
+        console.log("This is the interceptor for request");
+
+        console.log({ context });
+        
+        
+        return next.handle().pipe(
+            map((data) => {
+                // You can modify the response data here if needed
+                console.log("This is the interceptor for response");
+                console.log({ data });
+                const response = {
+                    ...data,
+                    createAt : data.created_at,
+                }
+
+                delete response.created_at;
+                delete response.updated_at;
+
+                return response;
+            }),
+        );
     }
 }
