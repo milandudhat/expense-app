dd8619bcb9e07d80d5fdcce6254691f2082c89cf
milanp <milan.dudhat@cybercom.in>
2023-10-09 15:40:19 +0530

    wfo

--- src/summary/summary.module.ts
+++ src/summary/summary.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SummaryController } from './summary.controller';
 import { SummaryService } from './summary.service';
+import { ReportModule } from 'src/report/report.module';
+// import { ReportService } from 'src/report/report.service';
 
 @Module({
+  imports : [ ReportModule],
   controllers: [SummaryController],
-  providers: [SummaryService]
+  providers: [SummaryService],
+
 })
 export class SummaryModule {}
--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -4,8 +4,15 @@ import { ReportService } from 'src/report/report.service';
 
 @Injectable()
 export class SummaryService {
-    // constructor(private readonly reportService: ReportService) { }
+    constructor(private readonly reportService: ReportService) { }
     calculateSummary() {
-        
+
+        const allExpenses = this.reportService.getAllReports(ReportType.EXPENSE);
+
+        return  {
+            totalIncome: 100,
+            totalExpense: 10,
+            netIncome: 100 - 10
+        }
     }
 }
